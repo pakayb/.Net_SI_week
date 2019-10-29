3bf97a7ae85dab7e2aeb4907a04c65dd9c37daec
pakayb <pakayb@gmail.com>
2019-10-29 11:49:37 +0100

    First exerciser, create person class

---
+++ CreateClass/CreateClass/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CreateClass
+{
+    class Person
+    {
+        protected string Name { get; }
+        protected DateTime BirthDate { get; }
+        protected Gender Gender { get; }
+
+        public Person(string name, DateTime birthDate, Gender gender)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            BirthDate = birthDate;
+            Gender = gender;
+        }
+
+        public override string ToString()
+        {
+            return "Name: " + Name + " | " +"Gender: " + Gender + " | " + "Birth Date: "+ BirthDate.ToShortDateString();
+        }
+
+        static void Main(string[] args)
+        {
+            var barna = new Person("Barna", new DateTime(1996,1,2),Gender.Male );
+            var diana = new Person("Diana", DateTime.Now, Gender.Female);
+            Console.WriteLine(barna.ToString());
+            Console.WriteLine(diana.ToString());
+            Console.ReadLine();
+        }
+
+
+    }
+    internal enum Gender
+    {
+        Male,
+        Female,
+    }
+}
